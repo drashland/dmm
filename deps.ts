363ca815b2f3b13f5caa5b9d2ddab52df1bbaf0d
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-02 22:17:53 +0100

    Added deps.ts

---
+++ deps.ts
@@ -0,0 +1,4 @@
+export { VERSION as denoStdLatestVersion } from "https://deno.land/std@0.54.0/version.ts"
+
+import * as colours from "https://deno.land/std@0.54.0/fmt/colors.ts"
+export { colours }
