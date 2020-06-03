fdbd2ab3d8514765a60864b4f7877fbbe59ca3a3
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-03 01:29:29 +0100

    Commented out update tests for the time being

--- tests/update_test.ts
+++ tests/update_test.ts
@@ -1,61 +1,61 @@
 // Update a specific dep that can be updated
-import {assertEquals} from "../deps";
+import {assertEquals} from "../deps.ts";
 
-Deno.test({
-    name: 'Update | Single | Modules to Update Exist',
-    async fn(): Promise<void> {
-
-    }
-})
-
-// Update a specific dep that is already up to date
-Deno.test({
-    name: 'Update | Single | No Modules to Update',
-    async fn(): Promise<void> {
-
-    }
-})
-
-// Update many deps that can be updated
-Deno.test({
-    name: 'Update | Many | Modules to Update Exist',
-    async fn(): Promise<void> {
-
-    }
-})
-
-// Update many deps that are already up to date
-Deno.test({
-    name: 'Update | Many | No Modules to Update',
-    async fn(): Promise<void> {
-
-    }
-})
-
-Deno.test({
-    name: 'Update | All | Modules to Update Exist',
-    async fn(): Promise<void> {
-
-    }
-})
-
-Deno.test({
-    name: 'Update | All | Modules to Update Exist',
-    async fn(): Promise<void> {
-
-    }
-})
-
-Deno.test({
-    name: 'Update | Single | std',
-    async (): Promise<void> {
-
-    }
-})
-
-Deno.test({
-    name: 'Update | Single | 3rd Party',
-    async fn(): Promise<void> {
-
-    }
-})
+// Deno.test({
+//     name: 'Update | Single | Modules to Update Exist',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
+//
+// // Update a specific dep that is already up to date
+// Deno.test({
+//     name: 'Update | Single | No Modules to Update',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
+//
+// // Update many deps that can be updated
+// Deno.test({
+//     name: 'Update | Many | Modules to Update Exist',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
+//
+// // Update many deps that are already up to date
+// Deno.test({
+//     name: 'Update | Many | No Modules to Update',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
+//
+// Deno.test({
+//     name: 'Update | All | Modules to Update Exist',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
+//
+// Deno.test({
+//     name: 'Update | All | Modules to Update Exist',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
+//
+// Deno.test({
+//     name: 'Update | Single | std',
+//     async (): Promise<void> {
+//
+//     }
+// })
+//
+// Deno.test({
+//     name: 'Update | Single | 3rd Party',
+//     async fn(): Promise<void> {
+//
+//     }
+// })
