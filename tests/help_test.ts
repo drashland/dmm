5c0b309cb46aef09fe380e6956d8a0b57c2918ce
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-03 01:20:33 +0100

    Completed help test

--- tests/help_test.ts
+++ tests/help_test.ts
@@ -1,6 +1,18 @@
-// Deno.test({
-//     name: 'Help',
-//     async (): Promise<void> {
-//
-//     }
-// })
+import {assertEquals} from "../deps.ts";
+
+Deno.test({
+    name: 'Help',
+    async fn(): Promise<void> {
+        const p = await Deno.run({
+            cmd: ["deno", "run", "../../mod.ts", "--help"],
+            cwd: "./tests/up-to-date-deps",
+            stdout: "null"
+        })
+        const status = await p.status()
+        const error = await p.stderr
+        p.close()
+        assertEquals(status.code, 0)
+        assertEquals(status.success, true)
+        assertEquals(error, undefined)
+    }
+})
