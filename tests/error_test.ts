f681c9cd0421315022fd5299f718c53a3c79e98f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-09 03:00:31 +0100

    [issue-#5-improve-tests] Improved error test

--- tests/error_test.ts
+++ tests/error_test.ts
@@ -1,4 +1,4 @@
-import {assertEquals} from "../deps.ts";
+import { assertEquals, colours } from "../deps.ts";
 
 Deno.test({
     name: 'No Purpose',
@@ -6,13 +6,21 @@ Deno.test({
         const p = await Deno.run({
             cmd: ["deno", "run", "--allow-net", "--allow-read", "../../mod.ts"],
             cwd: "./tests/up-to-date-deps",
-            stdout: "null"
+            stdout: "piped",
+            stderr: "piped"
         })
         const status = await p.status()
-        p.close()
         assertEquals(status.code, 1)
         assertEquals(status.success, false)
+        const output = await p.output()
+        await p.close()
+        const stdout = new TextDecoder('utf-8').decode(output)
+        const error = await p.stderrOutput()
+        const stderr = new TextDecoder('utf-8').decode(error)
+        assertEquals(stdout, '')
+        assertEquals(stderr, colours.red('Invalid arguments. See --help') + '\n')
     }
+
 })
 
 Deno.test({
@@ -21,11 +29,18 @@ Deno.test({
         const p = await Deno.run({
             cmd: ["deno", "run", "--allow-net", "--allow-read", "../../mod.ts", "something"],
             cwd: "./tests/up-to-date-deps",
-            stdout: "null"
+            stdout: "piped",
+            stderr: "piped"
         })
         const status = await p.status()
-        p.close()
         assertEquals(status.code, 1)
         assertEquals(status.success, false)
+        const output = await p.output()
+        await p.close()
+        const stdout = new TextDecoder('utf-8').decode(output)
+        const error = await p.stderrOutput()
+        const stderr = new TextDecoder('utf-8').decode(error)
+        assertEquals(stdout, 'Gathering facts...\n')
+        assertEquals(stderr, colours.red('Specify either `check`, `info` or `update`. See --help') + '\n')
     }
 })
