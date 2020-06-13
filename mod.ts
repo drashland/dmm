3704440aebce855149ce0173b467436b165e8203
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-13 21:04:20 +0100

    [refactor] Improved mod.ts

--- mod.ts
+++ mod.ts
@@ -4,25 +4,27 @@ import {
 } from "./dmm.ts";
 import { colours } from "./deps.ts";
 
-// Gather facts
+// Gather args
 const args: string[] = Deno.args;
 if (!args.length) {
   console.error(colours.red("Invalid arguments. See --help"));
   Deno.exit(1);
 }
+
+// Support --help usage
 const wantsHelp: boolean = args.filter((arg) => arg === "--help").length === 1;
-if (!wantsHelp) console.info("Gathering facts...");
+if (wantsHelp) {
+  console.info(helpMessage);
+  Deno.exit();
+}
+
+// Gather facts
+console.info("Gathering facts...");
 const purposeAndModules: string[] = args.filter((arg) =>
   arg.indexOf("--") === -1
 );
 const purpose: string = purposeAndModules[0];
 const modulesForPurpose: string[] = purposeAndModules.slice(1);
-
-// Support --help usage
-if (wantsHelp) {
-  console.info(helpMessage);
-  Deno.exit();
-}
 
 // Error when a supported purpose isn't found
 // Must be after the help logic because --help isn't really a supported purpose
