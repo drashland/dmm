341142c260affb011604ebd345f02262ec977da1
Mohamed ELHIRECH <mohamed.elhirech@prestataire.sacd.fr>
2020-06-18 15:32:08 +0200

    change version_test.ts check version using regex

--- tests/version_test.ts
+++ tests/version_test.ts
@@ -12,14 +12,20 @@ Deno.test({
     const status = await p.status();
     const output = await p.output();
     await p.close();
-    const stdout = new TextDecoder("utf-8").decode(output);
+    const stdout = new TextDecoder("utf-8").decode(output).replace(
+      /(\r\n|\n|\r)/gm,
+      "",
+    ); // regex to strip line break
     const error = await p.stderrOutput();
     const stderr = new TextDecoder("utf-8").decode(error);
+    // reference: https://regexr.com/39s32
+    const reg =
+      /^((([0-9]+)\.([0-9]+)\.([0-9]+)(?:-([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$/;
+    const splittedStdout = stdout.split(" ");
+    assertEquals(splittedStdout.length, 2);
+    assertEquals(splittedStdout[0], "dmm");
+    assertEquals(reg.test(splittedStdout[1]), true);
     assertEquals(stderr, "");
-    assertEquals(
-      stdout,
-      "dmm 1.0.5\n",
-    );
     assertEquals(status.code, 0);
     assertEquals(status.success, true);
   },
