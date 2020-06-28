984b65b8faab36cac7f67f0dfe96c5e4d36f99c9
Edward Bebbington <edward.bebbington@intercity.technology>
2020-06-28 21:15:57 +0100

    [issue-#3-update-tests] Updated dependencies that are supposed to be latest version

--- tests/up-to-date-deps/deps.ts
+++ tests/up-to-date-deps/deps.ts
@@ -1,7 +1,7 @@
 import { Drash } from "https://deno.land/x/drash@v1.0.6/mod.ts"; // up to date
 
-import * as fs from "https://deno.land/std@0.58.0/fs/mod.ts"; // up to date
+import * as fs from "https://deno.land/std@0.59.0/fs/mod.ts"; // up to date
 
-import * as colors from "https://deno.land/std@0.58.0/fmt/colors.ts"; // up to date
+import * as colors from "https://deno.land/std@0.59.0/fmt/colors.ts"; // up to date
 
 export { Drash, fs, colors };
