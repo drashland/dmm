6a5a54328a359cbae636476ba9e717e6887a453c
Eric Crooks <eric.crooks@gmail.com>
2020-07-23 18:55:28 -0400

    fix tests

--- tests/up-to-date-deps/deps.ts
+++ tests/up-to-date-deps/deps.ts
@@ -1,9 +1,9 @@
-import { Drash } from "https://deno.land/x/drash@v1.0.5/mod.ts"; // up to date
+import { Drash } from "https://deno.land/x/drash@v1.1.1/mod.ts"; // up to date
 
-import * as fs from "https://deno.land/std@0.57.0/fs/mod.ts"; // up to date
+import * as fs from "https://deno.land/std@0.61.0/fs/mod.ts"; // up to date
 
-import * as colors from "https://deno.land/std@0.57.0/fmt/colors.ts"; // up to date
+import * as colors from "https://deno.land/std@0.61.0/fmt/colors.ts"; // up to date
 
-import { Drash as drash } from "https://deno.land/x/drash@v1.0.5/mod.ts"; // up to date
+import { Drash as drash } from "https://deno.land/x/drash@v1.1.1/mod.ts"; // up to date
 
 export { Drash, fs, colors };
