31f8deba8b276a085d2032695372dc35e912b7f4
Edward Bebbington <edward.bebbington@intercity.technology>
2020-09-13 10:45:42 +0100

    Minor corrections

--- tests/integration/up-to-date-deps/deps.ts
+++ tests/integration/up-to-date-deps/deps.ts
@@ -4,4 +4,6 @@ import * as fs from "https://deno.land/std@0.68.0/fs/mod.ts"; // up to date
 
 import * as colors from "https://deno.land/std@0.68.0/fmt/colors.ts"; // up to date
 
+export { v4 } from "https://deno.land/std@0.68.0/uuid/mod.ts"; // up to date
+
 export { Drash, fs, colors };
