d46ca9281104e787cf28bacda54c4f2239178266
Edward Bebbington <edward.bebbington@intercity.technology>
2020-09-14 09:45:32 +0100

    Update dep version strings

--- tests/integration/up-to-date-deps/deps.ts
+++ tests/integration/up-to-date-deps/deps.ts
@@ -1,9 +1,9 @@
 import { Drash } from "https://deno.land/x/drash@v1.2.3/mod.ts"; // up to date
 
-import * as fs from "https://deno.land/std@0.68.0/fs/mod.ts"; // up to date
+import * as fs from "https://deno.land/std@0.69.0/fs/mod.ts"; // up to date
 
-import * as colors from "https://deno.land/std@0.68.0/fmt/colors.ts"; // up to date
+import * as colors from "https://deno.land/std@0.69.0/fmt/colors.ts"; // up to date
 
-export { v4 } from "https://deno.land/std@0.68.0/uuid/mod.ts"; // up to date
+export { v4 } from "https://deno.land/std@0.69.0/uuid/mod.ts"; // up to date
 
 export { Drash, fs, colors };
