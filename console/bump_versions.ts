d05ea2adabe7b917fc193af9746c5d6ff576e2cb
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-20 22:38:24 +0100

    Add pre release workflow to bump versions

---
+++ console/bump_versions.ts
@@ -0,0 +1,22 @@
+import { bumpVersions } from "https://raw.githubusercontent.com/drashland/services/master/console/bump_versions.ts";
+
+const branch: string = Deno.args[0].split("=")[1]; // ["--version", "release-vX.X.X"]
+const version = branch.substring(branch.indexOf("v") + 1); // 1.0.5
+
+bumpVersions([
+  {
+    filename: "./egg.json",
+    replaceTheRegex: /"version": "[0-9\.]+[0-9\.]+[0-9\.]"/,
+    replaceWith: `"version": "${version}"`
+  },
+  {
+    filename: "./README.md",
+    replaceTheRegex: /dmm@v[0-9\.]+[0-9\.]+[0-9\.]/g,
+    replaceWith: `dmm@v${version}`
+  },
+  {
+    filename: "./src/commands/version.ts",
+    replaceTheRegex: /version = "[0-9\.]+[0-9\.]+[0-9\.]"/,
+    replaceWith: `version = "${version}"`
+  }
+])
