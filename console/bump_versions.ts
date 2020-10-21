98e3c374c73b50d92a201c021131a5dc1ab35494
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-21 09:13:50 +0100

    Improve bump versions script

--- console/bump_versions.ts
+++ console/bump_versions.ts
@@ -1,22 +1,19 @@
 import { bumpVersions } from "https://raw.githubusercontent.com/drashland/services/master/console/bump_versions.ts";
 
-const branch: string = Deno.args[0].split("=")[1]; // ["--version", "release-vX.X.X"]
-const version = branch.substring(branch.indexOf("v") + 1); // 1.0.5
-
-bumpVersions([
+bumpVersions(Deno.args, [
   {
     filename: "./egg.json",
     replaceTheRegex: /"version": "[0-9\.]+[0-9\.]+[0-9\.]"/,
-    replaceWith: `"version": "${version}"`,
+    replaceWith: `"version": "{{ version }}"`,
   },
   {
     filename: "./README.md",
     replaceTheRegex: /dmm@v[0-9\.]+[0-9\.]+[0-9\.]/g,
-    replaceWith: `dmm@v${version}`,
+    replaceWith: `dmm@v{{ version }}`,
   },
   {
     filename: "./src/commands/version.ts",
     replaceTheRegex: /version = "[0-9\.]+[0-9\.]+[0-9\.]"/,
-    replaceWith: `version = "${version}"`,
+    replaceWith: `version = "{{ version }}"`,
   },
 ]);
