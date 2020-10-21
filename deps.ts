d0d21453862f4a4289d3624fa80609ec278ba260
Eric Crooks <eric.crooks@gmail.com>
2020-10-20 21:30:24 -0400

    use CommandService

--- deps.ts
+++ deps.ts
@@ -1,10 +1,7 @@
 import * as colours from "https://deno.land/std@0.74.0/fmt/colors.ts";
 export { colours };
 export { createHelpMenu } from "https://raw.githubusercontent.com/drashland/services/master/cli/help_menu_service.ts";
-export {
-  commandIs,
-  commandRequiresArgs,
-} from "https://raw.githubusercontent.com/drashland/services/master/cli/command_service.ts";
+export { CommandService } from "https://raw.githubusercontent.com/drashland/services/master/cli/command_service.ts";
 export {
   logDebug,
   logError,
