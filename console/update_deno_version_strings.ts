a7dcc8fe6607effb4919947a72a891eb92c3f127
Edward Bebbington <edward.bebbington@intercity.technology>
2020-10-21 10:22:53 +0100

    deno fmt

--- console/update_deno_version_strings.ts
+++ console/update_deno_version_strings.ts
@@ -4,23 +4,23 @@ bumper([
   {
     filename: "./.github/workflows/master.yml",
     replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-    replaceWith: `deno: ["{{ latestDenoVersion }}"]`
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
   },
   {
     filename: "./.github/workflows/bumper.yml",
     replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-    replaceWith: `deno: ["{{ latestDenoVersion }}"]`
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
   },
   {
     filename: "./tests/integration/up-to-date-deps/original_deps.ts",
     replaceTheRegex: /std@[0-9.]+[0-9.]+[0-9]/g,
-    replaceWith: "std@{{ latestStdVersion }}"
+    replaceWith: "std@{{ latestStdVersion }}",
   },
   {
     filename: "./tests/integration/up-to-date-deps/original_deps.ts",
     replaceTheRegex: /drash@v[0-9.]+[0-9.]+[0-9]/g,
-    replaceWith: "drash@v{{ latestDrashVersion }}"
-  }
-])
+    replaceWith: "drash@v{{ latestDrashVersion }}",
+  },
+]);
 
 //Deno.copyFileSync("./tests/integration/up-to-date-deps/original_deps.ts", "./tests/integration/up-to-date-deps/deps.ts")
