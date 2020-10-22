4d59609de49e2315dca2fc769c7a6266705dc898
Eric Crooks <eric.crooks@gmail.com>
2020-10-21 22:48:09 -0400

    test bumper script process

--- console/bumper_ci_service.ts
+++ console/bumper_ci_service.ts
@@ -1,53 +1,12 @@
 import { BumperService } from "https://raw.githubusercontent.com/drashland/services/master/ci/bumper_service.ts";
+import { denoVersionFiles, moduleVersionFiles } "./bumper_ci_service_files.ts";
 
 const b = new BumperService("dmm", Deno.args);
 
 if (b.isForPreRelease()) {
-  b.bump([
-    {
-      filename: "./egg.json",
-      replaceTheRegex: /"version": "[0-9\.]+[0-9\.]+[0-9\.]"/,
-      replaceWith: `"version": "{{ thisModulesLatestVersion }}"`,
-    },
-    {
-      filename: "./README.md",
-      replaceTheRegex: /dmm@v[0-9\.]+[0-9\.]+[0-9\.]/g,
-      replaceWith: `dmm@v{{ thisModulesLatestVersion }}`,
-    },
-    {
-      filename: "./src/commands/version.ts",
-      replaceTheRegex: /version = "[0-9\.]+[0-9\.]+[0-9\.]"/,
-      replaceWith: `version = "{{ thisModulesLatestVersion }}"`,
-    },
-  ]);
+  b.bump(moduleVersionFiles);
 } else {
-  b.bump([
-    {
-      filename: "./tests/integration/up-to-date-deps/original_deps.ts",
-      replaceTheRegex: /std@[0-9.]+[0-9.]+[0-9]/g,
-      replaceWith: "std@{{ latestStdVersion }}",
-    },
-    {
-      filename: "./tests/integration/up-to-date-deps/original_deps.ts",
-      replaceTheRegex: /drash@v[0-9.]+[0-9.]+[0-9]/g,
-      replaceWith: "drash@v{{ latestDrashVersion }}",
-    },
-    {
-      filename: "./.github/workflows/master.yml",
-      replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-      replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
-    },
-    {
-      filename: "./.github/workflows/bumper.yml",
-      replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-      replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
-    },
-    {
-      filename: "./.github/workflows/pre_release.yml",
-      replaceTheRegex: /deno: ["[0-9.]+[0-9.]+[0-9]"]/g,
-      replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
-    },
-  ]);
+  b.bump(denoVersionFiles);
   Deno.copyFileSync(
     "./tests/integration/up-to-date-deps/original_deps.ts",
     "./tests/integration/up-to-date-deps/deps.ts",
---
+++ console/bumper_ci_service_files.ts
@@ -0,0 +1,56 @@
+export const regexes = {
+  const_statements: /version = ".+"/g,
+  deps_drash: /drash@v[0-9.]+[0-9.]+[0-9]/g,
+  deps_std: /std@[0-9.]+[0-9.]+[0-9]/g,
+  egg_json: /"version": ".+"/,
+  urls: /dmm@v[0-9\.]+[0-9\.]+[0-9\.]/g,
+  yml_deno: /deno: \[".+"\]/g,
+};
+
+export const moduleVersionFiles = [
+  {
+    filename: "./egg.json",
+    replaceTheRegex: regexes.egg_json,
+    replaceWith: `"version": "{{ thisModulesLatestVersion }}"`,
+  },
+  {
+    filename: "./README.md",
+    replaceTheRegex: regexes.urls,
+    replaceWith: `dmm@v{{ thisModulesLatestVersion }}`,
+  },
+  {
+    filename: "./src/commands/version.ts",
+    replaceTheRegex: regexes.const_statements,
+    replaceWith: `version = "{{ thisModulesLatestVersion }}"`,
+  },
+];
+
+export const denoVersionFiles = [
+  {
+    filename: "./tests/integration/up-to-date-deps/original_deps.ts",
+    replaceTheRegex: regexes.deps_std,
+    replaceWith: "std@{{ latestStdVersion }}",
+  },
+  // Yes... this is NOT a deno version file, but we're trying to keep the tests
+  // up to date and it has a Drash version... so gtfo... leave this alone kthx.
+  {
+    filename: "./tests/integration/up-to-date-deps/original_deps.ts",
+    replaceTheRegex: regexes.deps_drash,
+    replaceWith: "drash@v{{ latestDrashVersion }}",
+  },
+  {
+    filename: "./.github/workflows/master.yml",
+    replaceTheRegex: regexes.yml_deno,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+  {
+    filename: "./.github/workflows/bumper.yml",
+    replaceTheRegex: regexes.yml_deno,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+  {
+    filename: "./.github/workflows/pre_release.yml",
+    replaceTheRegex: regexes.yml_deno,
+    replaceWith: `deno: ["{{ latestDenoVersion }}"]`,
+  },
+];
