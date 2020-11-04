cd6ec817cca39fbfe4d061a724fbee523d2decb0
Eric Crooks <eric.crooks@gmail.com>
2020-11-03 19:13:42 -0500

    bump versions

--- tests/integration/up-to-date-deps/deps.ts
+++ tests/integration/up-to-date-deps/deps.ts
@@ -1,8 +1,8 @@
-export { v4 } from "https://x.nest.land/std@0.76.0/uuid/mod.ts"; //up to date
 import * as Cliffy from "https://x.nest.land/cliffy@0.15.0/mod.ts"; //up to date
 import * as colors from "https://deno.land/std@0.76.0/fmt/colors.ts"; //up to date
 import * as fs from "https://deno.land/std@0.76.0/fs/mod.ts"; // up to date
 import * as log from "https://x.nest.land/std@0.76.0/log/mod.ts"; //up to date
 import { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts"; // up to date
 
+export { v4 } from "https://x.nest.land/std@0.76.0/uuid/mod.ts"; //up to date
 export { Cliffy, colors, Drash, fs, log };
