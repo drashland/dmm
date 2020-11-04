ba9a6ecc17ac10ee7ba7eea568236ffde778e75a
Eric Crooks <eric.crooks@gmail.com>
2020-11-03 20:29:12 -0500

    fix comments

--- tests/integration/up-to-date-deps/deps.ts
+++ tests/integration/up-to-date-deps/deps.ts
@@ -1,13 +1,13 @@
-import { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts"; // out of date
+import { Drash } from "https://deno.land/x/drash@v1.2.5/mod.ts"; // up to date
 
-import * as fs from "https://deno.land/std@0.76.0/fs/mod.ts"; // out of date
+import * as fs from "https://deno.land/std@0.76.0/fs/mod.ts"; // up to date
 
-import * as colors from "https://deno.land/std@0.76.0/fmt/colors.ts"; //out of date
+import * as colors from "https://deno.land/std@0.76.0/fmt/colors.ts"; //up to date
 
-import * as Cliffy from "https://x.nest.land/cliffy@0.15.0/mod.ts"; //out of date
+import * as Cliffy from "https://x.nest.land/cliffy@0.15.0/mod.ts"; //up to date
 
-import * as log from "https://deno.land/std@0.76.0/log/mod.ts"; //out of date
+import * as log from "https://deno.land/std@0.76.0/log/mod.ts"; //up to date
 
-export { v4 } from "https://deno.land/std@0.76.0/uuid/mod.ts"; //out of date
+export { v4 } from "https://deno.land/std@0.76.0/uuid/mod.ts"; //up to date
 
 export { Cliffy, colors, Drash, fs, log };
