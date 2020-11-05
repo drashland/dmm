968ddecb9fdc86fa653d0dc69bdf11357663d23f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-05 16:26:46 +0000

    [fix-egg-and-tests] Fix tests again

--- tests/integration/out-of-date-deps/deps.ts
+++ tests/integration/out-of-date-deps/deps.ts
@@ -1,6 +1,6 @@
 import { Drash } from "https://deno.land/x/drash@v1.0.0/mod.ts"; // out of date
 
-import * as fs from "https://deno.land/std@0.76.0/fs/mod.ts"; // out of date
+import * as fs from "https://deno.land/std@0.53.0/fs/mod.ts"; // out of date
 
 import * as colors from "https://deno.land/std@0.53.0/fmt/colors.ts"; //out of date
 
