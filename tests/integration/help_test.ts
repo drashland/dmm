e4e13937b90bce09e246089f5f03eda6b687a77f
Edward Bebbington <edward.bebbington@intercity.technology>
2020-11-22 22:54:28 +0000

    [minor-chores] Reduce amount of files needed in bumper script

--- tests/integration/help_test.ts
+++ tests/integration/help_test.ts
@@ -1,6 +1,7 @@
 import { assertEquals, colours } from "../../deps.ts";
 import { version } from "../../src/commands/version.ts";
 import { outOfDateDepsDir, upToDateDepsDir } from "./test_constants.ts";
+import {expectedHelpMessage} from "../data/expected_help_message.ts";
 
 Deno.test({
   name: "Help",
@@ -20,50 +21,7 @@ Deno.test({
     assertEquals(stderr, "");
     assertEquals(
       stdout,
-      `
-A module manager for Deno.
-
-USAGE
-
-    deno install --allow-net='x.nest.land,cdn.deno.land,api.deno.land,raw.githubusercontent.com,github.com,api.github.com' --allow-read='.' --allow-write='deps.ts' https://deno.land/x/dmm@v1.3.0/mod.ts
-    dmm [SUBCOMMAND]
-
-SUBCOMMANDS
-
-    check [modules]
-        Checks the specified modules for newer version. Will check all if modules are 
-        omitted.
-
-    update [modules]
-        Updates the specified modules to the newest version. Will update all if modules 
-        are omitted.
-
-    info [modules]
-        Displays information about the given modules, be it std or 3rd party. The 3rd 
-        party module must be referenced at https://deno.land/x/
-
-    help, --help
-        Prints the help message
-
-    version, --version
-        Prints the current dmm version
-
-
-EXAMPLE USAGE
-
-    Install dmm
-        deno install --allow-net='x.nest.land,cdn.deno.land,api.deno.land,raw.githubusercontent.com,github.com,api.github.com' --allow-read='.' --allow-write='deps.ts' https://deno.land/x/dmm@v1.3.0/mod.ts
-
-    Check a single module
-        dmm check fs
-
-    Update a single module
-        dmm update fs
-
-    Get information about a module
-        dmm info http
-
-`,
+      expectedHelpMessage,
     );
     assertEquals(status.code, 0);
     assertEquals(status.success, true);
