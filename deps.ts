e268ca673939ba55ed38237884542f7e71ac069d
crookse <crookse@users.noreply.github.com>
2021-04-14 00:51:40 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { BumperService } from "https://raw.githubusercontent.com/drashland/services/v0.2.1/ci/bumper_service.ts";
 export { CliService } from "https://raw.githubusercontent.com/drashland/services/v0.2.1/cli/cli_service.ts";
 export { ConsoleLogger } from "https://raw.githubusercontent.com/drashland/services/v0.2.1/loggers/console_logger.ts";
-export { assertEquals } from "https://deno.land/std@0.92.0/testing/asserts.ts";
-import * as colours from "https://deno.land/std@0.92.0/fmt/colors.ts";
+export { assertEquals } from "https://deno.land/std@0.93.0/testing/asserts.ts";
+import * as colours from "https://deno.land/std@0.93.0/fmt/colors.ts";
 export { colours };
--- tests/integration/up-to-date-deps/deps.ts
+++ tests/integration/up-to-date-deps/deps.ts
@@ -1,14 +1,14 @@
 import { Drash } from "https://deno.land/x/drash@v1.4.4/mod.ts"; // up to date
 
-import * as fs from "https://deno.land/std@0.92.0/fs/mod.ts"; // up to date
+import * as fs from "https://deno.land/std@0.93.0/fs/mod.ts"; // up to date
 
-import * as colors from "https://deno.land/std@0.92.0/fmt/colors.ts"; //up to date
+import * as colors from "https://deno.land/std@0.93.0/fmt/colors.ts"; //up to date
 
-import * as Cliffy from "https://x.nest.land/cliffy@0.18.1/mod.ts"; //up to date
+import * as Cliffy from "https://x.nest.land/cliffy@0.18.2/mod.ts"; //up to date
 
-import * as log from "https://deno.land/std@0.92.0/log/mod.ts"; //up to date
+import * as log from "https://deno.land/std@0.93.0/log/mod.ts"; //up to date
 
-export { v4 } from "https://deno.land/std@0.92.0/uuid/mod.ts"; //up to date
+export { v4 } from "https://deno.land/std@0.93.0/uuid/mod.ts"; //up to date
 
 export { Cliffy, colors, Drash, fs, log };
 
