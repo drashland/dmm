155c954579458d6bf8cc7e2bdb66242ba71847c6
Edward Bebbington <EdwardSBebbington@hotmail.com>
2021-10-07 22:29:59 +0100

    fmt

--- deps.ts
+++ deps.ts
@@ -4,5 +4,5 @@ import { ConsoleLogger } from "https://deno.land/x/unilogger@v1.0.1/mod.ts";
 const consoleLogger = new ConsoleLogger({});
 export { consoleLogger as ConsoleLogger };
 export { assertEquals } from "https://deno.land/std@0.110.0/testing/asserts.ts";
-import * as colours from "https://deno.land/std@0.110.0/fmt/colors.ts";;
+import * as colours from "https://deno.land/std@0.110.0/fmt/colors.ts";
 export { colours };
