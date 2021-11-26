af6b4a46d83fde9b1bf8ded4995ee67ac278265c
Eric Crooks <eric.crooks@gmail.com>
2021-11-26 17:26:57 -0500

    chore: lint

--- tests/unit/console/bumper_ci_service_test.ts
+++ tests/unit/console/bumper_ci_service_test.ts
@@ -2,14 +2,8 @@ import { assertEquals } from "../../../deps.ts";
 import { preReleaseFiles } from "../../../console/bumper_ci_service_files.ts";
 import { BumperService } from "../../../deps.ts";
 
-// Not for pre-release
-const b = new BumperService("dmm", Deno.args);
-
 // For pre-release
 const c = new BumperService("dmm", ["--version=1.2.3"]);
-
-const latestVersions = await b.getLatestVersions();
-const latestVersionDrash = await c.getModulesLatestVersion("drash");
 
 Deno.test({
   name: "Bumper CI Service | bumps eggs.json correctly",
@@ -34,114 +28,6 @@ Deno.test({
 ////////////////////////////////////////////////////////////////////////////////
 // DATA PROVIDERS //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
-// deno-lint-ignore camelcase
-const data_depsStd = `workflow files
-
-deno: ["0.0.0"]
-deno: ["0.0.0"]
-
------
-
-urls
-
-https://deno.land/x/dmm@v0.0.0/mod.ts
-https://deno.land/x/dmm@v0.0.0/mod.ts
-
-----
-
-consts
-
-export const version = "0.0.0";
-export const version = "0.0.0";
-
-----
-
-egg.json
-
-"version": "0.0.0",
-
-----
-
-deps files
-
-import { Drash } from "https://deno.land/x/drash@v0.0.0/mod.ts"; // up to date
-import * as fs from "https://deno.land/std@${latestVersions.deno_std}/fs/mod.ts"; // up to date
-import * as colors from "https://deno.land/std@${latestVersions.deno_std}/fmt/colors.ts"; // up to date
-export { v4 } from "https://deno.land/std@${latestVersions.deno_std}/uuid/mod.ts"; // up to date
-`;
-
-// deno-lint-ignore camelcase
-const data_depsDrash = `workflow files
-
-deno: ["0.0.0"]
-deno: ["0.0.0"]
-
------
-
-urls
-
-https://deno.land/x/dmm@v0.0.0/mod.ts
-https://deno.land/x/dmm@v0.0.0/mod.ts
-
-----
-
-consts
-
-export const version = "0.0.0";
-export const version = "0.0.0";
-
-----
-
-egg.json
-
-"version": "0.0.0",
-
-----
-
-deps files
-
-import { Drash } from "https://deno.land/x/drash@v${latestVersionDrash}/mod.ts"; // up to date
-import * as fs from "https://deno.land/std@0.0.0/fs/mod.ts"; // up to date
-import * as colors from "https://deno.land/std@0.0.0/fmt/colors.ts"; // up to date
-export { v4 } from "https://deno.land/std@0.0.0/uuid/mod.ts"; // up to date
-`;
-
-// deno-lint-ignore camelcase
-const data_denoVersionsYml = `workflow files
-
-deno: ["${latestVersions.deno}"]
-deno: ["${latestVersions.deno}"]
-
------
-
-urls
-
-https://deno.land/x/dmm@v0.0.0/mod.ts
-https://deno.land/x/dmm@v0.0.0/mod.ts
-
-----
-
-consts
-
-export const version = "0.0.0";
-export const version = "0.0.0";
-
-----
-
-egg.json
-
-"version": "0.0.0",
-
-----
-
-deps files
-
-import { Drash } from "https://deno.land/x/drash@v0.0.0/mod.ts"; // up to date
-import * as fs from "https://deno.land/std@0.0.0/fs/mod.ts"; // up to date
-import * as colors from "https://deno.land/std@0.0.0/fmt/colors.ts"; // up to date
-export { v4 } from "https://deno.land/std@0.0.0/uuid/mod.ts"; // up to date
-`;
 
 // deno-lint-ignore camelcase
 const data_constStatements = `workflow files
