99b3eaf56bc087f99cc75145bd9cc4d86e12b39a
drashbot <59404681+drashbot@users.noreply.github.com>
2021-11-25 21:24:01 -0500

    Update dependencies and bump version numbers across repo (#120)
    Co-authored-by: crookse <crookse@users.noreply.github.com>

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { BumperService } from "https://raw.githubusercontent.com/drashland/services/v0.2.4/ci/bumper_service.ts";
 export { Line, Subcommand } from "https://deno.land/x/line@v0.1.1/mod.ts";
 export { ConsoleLogger } from "https://raw.githubusercontent.com/drashland/services/v0.2.4/loggers/console_logger.ts";
-export { assertEquals } from "https://deno.land/std@0.115.1/testing/asserts.ts";
-import * as colours from "https://deno.land/std@0.115.1/fmt/colors.ts";
+export { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
+import * as colours from "https://deno.land/std@0.116.0/fmt/colors.ts";
 export { colours };
