c35dbc4eec140dc7aea33803ce1a022950ad7050
crookse <crookse@users.noreply.github.com>
2021-12-14 00:53:58 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,5 +1,5 @@
 export { BumperService } from "https://raw.githubusercontent.com/drashland/services/v0.2.4/ci/bumper_service.ts";
-export { Line, Subcommand } from "https://deno.land/x/line@v0.1.1/mod.ts";
+export { Line, Subcommand } from "https://deno.land/x/line@v1.0.0/mod.ts";
 export { ConsoleLogger } from "https://raw.githubusercontent.com/drashland/services/v0.2.4/loggers/console_logger.ts";
 export { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 import * as colours from "https://deno.land/std@0.117.0/fmt/colors.ts";
