dc2179a9d33afea6c4b8116c8c56552a6bd21db1
crookse <crookse@users.noreply.github.com>
2022-01-21 00:49:20 +0000

    Update dependencies and bump version numbers across repo

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export { BumperService } from "https://raw.githubusercontent.com/drashland/services/v0.2.4/ci/bumper_service.ts";
 export * as Line from "https://deno.land/x/line@v1.0.0/mod.ts";
-import { ConsoleLogger } from "https://deno.land/x/unilogger@v1.0.3/mod.ts";
+import { ConsoleLogger } from "https://deno.land/x/unilogger@v1.0.4/mod.ts";
 const consoleLogger = new ConsoleLogger({});
 export { consoleLogger as ConsoleLogger };
-export { assertEquals } from "https://deno.land/std@0.119.0/testing/asserts.ts";
-export * as colours from "https://deno.land/std@0.119.0/fmt/colors.ts";
+export { assertEquals } from "https://deno.land/std@0.122.0/testing/asserts.ts";
+export * as colours from "https://deno.land/std@0.122.0/fmt/colors.ts";
