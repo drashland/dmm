f3e11675db1f5108af896136d8beb79d2eca155e
Edward Bebbington <47337480+ebebbington@users.noreply.github.com>
2022-01-27 23:43:51 +0000

    Update version.ts

--- src/commands/version.ts
+++ src/commands/version.ts
@@ -1,3 +1,3 @@
 // TODO(crookse): Use the same logic that Drash uses on its `version` property
-export const version = "2.0.0";
+export const version = "2.0.1";
 export const versionMessage = `dmm ${version}`;
