54db559d1d0e23e9bde6828c3646cb656ed5306b
drashbot <59404681+drashbot@users.noreply.github.com>
2022-05-28 00:39:43 +0000

    chore: update deps

--- tests/integration/up-to-date-deps/original_deps.ts
+++ tests/integration/up-to-date-deps/original_deps.ts
@@ -1,17 +1,17 @@
 import { Drash } from "https://deno.land/x/drash@v2.6.0/mod.ts"; // up to date
 
-import * as fs from "https://deno.land/std@0.140.0/fs/mod.ts"; // up to date
+import * as fs from "https://deno.land/std@0.141.0/fs/mod.ts"; // up to date
 
-import * as colors from "https://deno.land/std@0.140.0/fmt/colors.ts"; //up to date
+import * as colors from "https://deno.land/std@0.141.0/fmt/colors.ts"; //up to date
 
 import * as Cliffy from "https://x.nest.land/cliffy@0.20.1/mod.ts"; //up to date
 
-import * as log from "https://deno.land/std@0.140.0/log/mod.ts"; //up to date
+import * as log from "https://deno.land/std@0.141.0/log/mod.ts"; //up to date
 
-export { v4 } from "https://deno.land/std@0.140.0/uuid/mod.ts"; //up to date
+export { v4 } from "https://deno.land/std@0.141.0/uuid/mod.ts"; //up to date
 
 export { Cliffy, colors, Drash, fs, log };
 
 export { Something } from "https://raw.githubusercontent.com/drashland/wocket/v1.0.0/mod.ts";
 
-export { createBot } from "https://deno.land/x/discordeno@13.0.0-rc39/mod.ts";
+export { createBot } from "https://deno.land/x/discordeno@13.0.0-rc43/mod.ts";
